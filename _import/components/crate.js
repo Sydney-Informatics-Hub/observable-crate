db7f61f5793878df1f1b3b1584d4435068e2461f
Mike Lynch <m.lynch@sydney.edu.au>
2024-03-30 14:56:37 +1100

    Build load-crate version

--- _import/components/crate.js
+++ _import/components/crate.js
@@ -14,8 +14,12 @@ export function nil_crate() {
 }
 
 export function root_entity(entities) {
-	const root_id = entities['ro-crate-metadata.json']['links_from']['about'][0];
-	return entities[root_id];
+	if( 'ro-crate-metadata.json' in entities ) {
+		const root_id = entities['ro-crate-metadata.json']['links_from']['about'][0];
+		return entities[root_id];
+	} else {
+		return null;
+	}
 }
 
 export function crate_link(entities, i) {
@@ -70,31 +74,16 @@ ${entity_links(nodes, "links_from", node)}
 }
 
 
-export function naive_tree(crate) {
-	const nodes = crate.nodes;
-	const links = crate.links;
-	const seen = new Set();
-	const tree = {};
-	const path_delim = "|";
-
-	const root = root_entity(crate.nodes);
-
-	tree[root.id] = root.name;
-
-	naive_tree_r(nodes, seen, root.id, tree, root.links_from);
-	return Object.keys(tree);
-}
-
-function naive_tree_r(nodes, seen, stem, tree, links) {
-	for( const prop in links ) {
-		for( const i of links[prop] ) {
-			if( !seen.has(i) ) {
-				seen.add(i);
-				const path = stem + '|' + nodes[i].name;
-				tree[path] = nodes[i].name;
-				naive_tree_r(nodes, seen, path, tree, nodes[i].links_from)
-			}
+export function current_crate() {
+	const current_json = sessionStorage.getItem("ro-crate");
+	if( current_json ) {
+		try {
+			const crate = JSON.parse(current_json);
+			return crate;
+		} catch(e) {
+			console.log("Bad result from sessionStorage")
+			return nil_crate();
 		}
 	}
+	return nil_crate();
 }
-
