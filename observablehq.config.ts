0c98b3cc55c4ac03b8eb1ef329f410265cc731b1
Mike Lynch <m.lynch@sydney.edu.au>
2024-10-08 14:30:18 +1100

    Scans a crate and copies all File entities across

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -1,10 +1,21 @@
 // See https://observablehq.com/framework/config for documentation.
 
 
+import fs from 'fs-extra';
+import {ROCrate} from 'ro-crate';
 
-// async function* get_crate_files() {
 
-// }
+async function* get_crate_files() {
+  const crateJson = await fs.readJson('./src/data/crate/ro-crate-metadata.json');
+  const crate = new ROCrate(crateJson);
+  for( const file_e of crate.graph.filter((e) => e['@type'] =='File') ) {
+    console.log(file_e['name']);
+    yield file_e['name'];
+  }
+}
+
+
+
 
 
 
@@ -16,9 +27,9 @@ export default {
     {name: "About", path: "/about.html"},
   ],
   async *dynamicPaths() { 
-    yield `/data/crate/0001fbbaaca311ec8db8f23c92f50f45.png`;
-    // for await( const {path} of get_crate_files() ) {
-    //   yield `/data/crate/${path}`;
-    // }
+    //yield `/data/crate/0001fbbaaca311ec8db8f23c92f50f45.png`;
+    for await( const path of get_crate_files() ) {
+       yield `/data/crate/${path}`;
+    }
   }
 };
