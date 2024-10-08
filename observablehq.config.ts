a00220df5cd46eeca91b236e902fe15518b75205
Mike Lynch <m.lynch@sydney.edu.au>
2024-10-09 10:30:40 +1100

    Trying to get static file copying to make sense

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -8,16 +8,20 @@ import {ROCrate} from 'ro-crate';
 async function* get_crate_files() {
   const crateJson = await fs.readJson('./src/data/crate/ro-crate-metadata.json');
   const crate = new ROCrate(crateJson);
+  // speed things up
+  const max = 20;
+  let i = 0;
   for( const file_e of crate.graph.filter((e) => e['@type'] =='File') ) {
     console.log(file_e['name']);
     yield file_e['name'];
+    i++;
+    if( i > max ) {
+      return;
+    }
   }
 }
 
-
-
-
-
+// the first time I did this it ended up in _files but now it doesn't?
 
 export default {
   title: "RO-Crate Explorer",
@@ -26,10 +30,15 @@ export default {
     {name: "Visual", path: "/visual.html"},
     {name: "About", path: "/about.html"},
   ],
-  async *dynamicPaths() { 
-    //yield `/data/crate/0001fbbaaca311ec8db8f23c92f50f45.png`;
-    for await( const path of get_crate_files() ) {
-       yield `/data/crate/${path}`;
-    }
-  }
+  dynamicPaths: [
+    'data/crate/800571f2cf9211ee8b63f23c92f50f45.png',
+    'data/crate/fffa406a249311ec84d2f23c92f50f45.png',
+    'data/crate/800e2bfc7f9511ec81e8f23c92f50f45.png', 
+    'data/crate/fffcbb78e14611ebba0af23c92f50f45.png',
+    ]
+  // async *dynamicPaths() { 
+  //   for await( const path of get_crate_files() ) {
+  //      yield `/data/crate/${path}`;
+  //   }
+  // }
 };
